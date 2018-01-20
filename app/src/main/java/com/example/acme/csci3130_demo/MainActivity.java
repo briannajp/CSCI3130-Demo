b6c122dab0db965e4f9f9b90cafc30c658900a8d
briannajp <32841437+briannajp@users.noreply.github.com>
2018-01-20 13:53:00 -0400

    Added new branch - newUIElements. Created button and new text field. Added ability to click button and place input into original textbox.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //initialize button and text boxes
+    EditText textInput;
+    Button buttonClick;
+    TextView textOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonClick = findViewById(R.id.buttonClick);
+        textInput = findViewById(R.id.editText);
+        textOutput = findViewById(R.id.textView);
     }
+
+    public void button_click(View view)
+    {
+        //swap whatever is in the edit text field into text view
+        textOutput.setText(textInput.getText());
+        textInput.setFocusable(false);
+    }
+
 }
